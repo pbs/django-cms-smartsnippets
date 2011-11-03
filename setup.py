53fbdd809cfec481eb47aca8f751c198acd1deb7
Sever Băneşiu <banesiu.sever@gmail.com>
2011-11-03 12:14:19 +0200

    Bumped version and added readme in setup.py description.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import os
 from setuptools import setup, find_packages
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'README.rst')
 
 setup(
     name='django-cms-smartsnippets',
-    version='0.1',
+    version='0.1.1',
     description='Parametrizable Django CMS snippets.',
+    long_description = open(README_PATH, 'r').read(),
     author='Sever Banesiu',
     author_email='banesiu.sever@gmail.com',
     packages=find_packages(),
