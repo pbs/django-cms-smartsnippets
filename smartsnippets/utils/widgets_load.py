f43a34d6bc7ef6886a1d662cce17468df57e059b
Paul Oprea <paul.oprea@threepillarglobal.com>
2012-04-11 17:16:05 +0300

    Refactoring the widgets for better integration

--- smartsnippets/utils/widgets_load.py
+++
@@ -1,7 +0,0 @@
-from cms.utils.django_load import get_module
-from smartsnippets.settings import installed_widgets
-
-
-def load(modname, verbose=False, failfast=False):
-    for widget in installed_widgets:
-        get_module(widget, modname, verbose, failfast)
--- smartsnippets/widgets.py
+++ smartsnippets/widgets.py
@@ -3,6 +3,22 @@ from smartsnippets.widgets_pool import widget_pool
 from smartsnippets.widgets_base import SmartSnippetWidgetBase
 
 
+class TextField(SmartSnippetWidgetBase):
+    name = 'Text Field'
+
+    def render(self):
+        return render_to_string('smartsnippets/widgets/textfield/widget.html',
+                                    {'field': self})
+
+
+class TextAreaField(SmartSnippetWidgetBase):
+    name = 'TextArea Field'
+
+    def render(self):
+        return render_to_string('smartsnippets/widgets/textareafield/widget.html',
+                                    {'field': self})
+
+
 class DropDownField(SmartSnippetWidgetBase):
     name = 'DropDown Field'
 
@@ -14,4 +30,6 @@ class DropDownField(SmartSnippetWidgetBase):
         return render_to_string('smartsnippets/widgets/dropdownfield/widget.html',
                                 {'field': self})
 
+widget_pool.register_widget(TextField)
+widget_pool.register_widget(TextAreaField)
 widget_pool.register_widget(DropDownField)
