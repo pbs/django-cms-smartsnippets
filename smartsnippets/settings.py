114f8503cdbdac9d1e89baf9408ac35ee8f57a8b
Alex Cucu <alexandruioan.cucu@gmail.com>
2012-10-26 10:33:20 +0300

    * cache rendered snippet content * add cache invalidation logic: whenever a snippet pointer, snippet, variable or variable value changes, invalidate the cache

--- smartsnippets/settings.py
+++ smartsnippets/settings.py
@@ -3,3 +3,6 @@ from django.conf import settings
 shared_sites = getattr(settings, 'SMARTSNIPPETS_SHARED_SITES', [])
 include_orphan = getattr(settings, 'SMARTSNIPPETS_INCLUDE_ORPHAN', True)
 restrict_user = getattr(settings, 'SMARTSNIPPETS_RESTRICT_USER', False)
+
+snippet_caching_time = getattr(settings, 'SMARTSNIPPETS_DEFAULT_CACHING_TIME', 3600)
+caching_enabled = snippet_caching_time != 0
