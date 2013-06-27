0947accab88f43e065eebc254be6f736cadb84f6
Laura Feier <laura.feier@threepillarglobal.com>
2013-06-27 19:13:06 +0300

    Bumped version for bamboo.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
 
 setup(
     name='django-cms-smartsnippets',
-    version='0.1.16',
+    version='0.1.17',
     description='Parametrizable Django CMS snippets.',
     long_description = open(README_PATH, 'r').read(),
     author='Sever Banesiu',
