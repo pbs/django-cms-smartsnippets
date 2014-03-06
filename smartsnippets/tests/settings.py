a342c8258854705333115968bb48dd83dfbb3172
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-03-06 17:05:43 +0200

    Add tests to smartsnippet caching restore.

--- smartsnippets/tests/settings.py
+++ smartsnippets/tests/settings.py
@@ -1,3 +1,4 @@
+import os
 from django.conf import settings
 
 shared_sites = getattr(settings, 'SMARTSNIPPETS_SHARED_SITES', [])
@@ -6,6 +7,29 @@ restrict_user = getattr(settings, 'SMARTSNIPPETS_RESTRICT_USER', False)
 
 snippet_caching_time = getattr(settings, 'SMARTSNIPPETS_CACHING_TIME', 300)
 caching_enabled = snippet_caching_time != 0
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ROOT_URLCONF = 'smartsnippets.tests.urls'
+CMS_TEMPLATES = [('sample.html',) * 2 ]
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'cms',
+    'mptt',
+    'south',
+    'sekizai',
+    'smartsnippets',
+)
+
 
 DATABASES = {
     'default': {
@@ -17,3 +41,28 @@ DATABASES = {
         'PORT' : '', # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+
+SECRET_KEY = 'c8syx=b9zy)+v$07wb+f!=&amp;#38w829lp*r$$%6i_6b@khg1-n5'
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
+)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+    'cms.context_processors.media',
+    'sekizai.context_processors.sekizai',
+)
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(HERE, 'templates',)
+)
--- smartsnippets/tests/tests.py
+++ smartsnippets/tests/tests.py
@@ -1,17 +1,83 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.sites.models import Site
+from django.template import RequestContext
+from django.core.cache import cache
+from django import http
 from django.test import TestCase
+from sekizai.helpers import get_varname as sekizai_key
 
 
-class SimpleTest(TestCase):
+from smartsnippets.models import (
+    SmartSnippet,
+    SmartSnippetPointer,
+)
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+def do_make_smartsnippet(template_code):
+    snippet = SmartSnippet.objects.create(
+        name='test_snippet',
+        template_code=template_code,
+    )
+    example_dot_com = Site.objects.get(pk=1, domain='example.com')
+    snippet.sites.add(example_dot_com)
+    return snippet
+
+
+def do_make_pointer(snippet):
+    pointer = SmartSnippetPointer.objects.create(snippet=snippet)
+    return pointer
+
+
+class NonStaffUser(object):
+    is_staff = False
+
+
+def make_context():
+    context, request = RequestContext({}), http.HttpRequest()
+    request.user = NonStaffUser()
+    context['request'] = request
+    return context
+
+
+def sekizai_context(context):
+    key = sekizai_key()
+    content = context[key]
+    return content
+
+
+class TestSekizaiCache(TestCase):
+    css_content = '<style type="text/css">body { color: red; }</style>'
+    header = '{% load sekizai_tags %}'
+    text = 'There is no spoon'
+    start = '{% addtoblock "css" %}'
+    end = '{% endaddtoblock %}'
+    template_code = ''.join([header, text, start, css_content, end])
+
+    def setUp(self):
+        self.snippet = do_make_smartsnippet(self.template_code)
+        self.snippet_pointer = do_make_pointer(self.snippet)
+
+    def test_sekizai_context_alteration(self):
+        context = make_context()
+        self.assertEqual(sekizai_context(context), {})
+        content = self.snippet_pointer.render(context)
+        self.assertEqual(content, self.text)
+        css_block = sekizai_context(context)['css']
+        self.assertEqual(css_block, [self.css_content])
+
+    def test_caching(self):
+        self.snippet_pointer.render(make_context())
+        cache_key = self.snippet_pointer.get_cache_key()
+        cache_value = cache.get(cache_key)
+        self.assertEqual(cache_value['content'], self.text)
+        self.assertEqual(cache_value['sekizai']['css'], [self.css_content])
+
+    def test_sekizai_restore(self):
+        self.snippet_pointer.render(make_context())
+        context = make_context()
+        content = self.snippet_pointer.render(context)
+        self.assertEqual(content, self.text)
+        css_block = sekizai_context(context)['css']
+        self.assertEqual(css_block, [self.css_content])
+
+    def tearDown(self):
+        cache.clear()
