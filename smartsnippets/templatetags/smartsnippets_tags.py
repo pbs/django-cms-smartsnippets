d9d1c2c815eea37dcd585e5bfe28511cd80e9b73
Laura Feier <laura.feier@3pillarglobal.com>
2014-11-19 16:41:20 +0200

    * implemented functionality to expose global settings in template. Due to security issues django settings should not get exposed in templates (secret credentials might get stolen). What will get exposed in snippets settings will be up to the developer's decision.

--- smartsnippets/templatetags/smartsnippets_tags.py
+++ smartsnippets/templatetags/smartsnippets_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from collections import OrderedDict
+from smartsnippets.widgets_pool import widget_pool
 
 register = template.Library()
 
@@ -6,3 +8,23 @@ register = template.Library()
 def render_variable(context, var):
     request = context['request']
     return var.render(request)
+
+
+@register.assignment_tag
+def settings_value(what):
+    return widget_pool.get_settings(*what.split('.', 1))
+
+
+@register.filter
+def sortdict(dict_to_sort):
+    return OrderedDict(sorted(dict_to_sort.items()))
+
+
+@register.filter
+def get_item(dictionary, key):
+    return (dictionary or {}).get(key, '')
+
+
+@register.filter
+def sortlist(list_to_sort):
+    return sorted(list(list_to_sort))
--- smartsnippets/widgets_pool.py
+++ smartsnippets/widgets_pool.py
@@ -3,18 +3,31 @@ from cms.utils.django_load import load
 from smartsnippets.exceptions import WidgetAlreadyRegistered, WidgetNotRegistered
 from smartsnippets.widgets_base import SmartSnippetWidgetBase
 from models import SmartSnippetVariable
+from collections import defaultdict
+
 
 class WidgetPool(object):
     def __init__(self):
         self.widgets = {}
+        self._settings = defaultdict(dict)
         self.discovered = False
-        
+
     def discover_widgets(self):
         if self.discovered:
             return
         self.discovered = True
         load('widgets')
-        
+
+    def register_settings(self, namespace, settings_dict):
+        self._settings[namespace].update(settings_dict)
+
+    def unregister_settings(self, namespace, settings_keys=None):
+        if settings_keys:
+            for item in settings_keys:
+                self._settings[namespace].pop(item, None)
+        else:
+            self._settings.pop(namespace, None)
+
     def register_widget(self, widget):
         if not issubclass(widget, SmartSnippetWidgetBase):
             raise ImproperlyConfigured(
@@ -27,10 +40,10 @@ class WidgetPool(object):
                 "Cannot register %s, a widget with this name (%s) is already "
                 "retgistered." % (widget, widget_name)
             )
-        
+
         widget.value = widget_name
         self.widgets[widget_name] = widget
-        
+
     def unregister_widget(self, widget):
         widget_name = widget.__name__
         if widget_name not in self.widgets:
@@ -38,15 +51,21 @@ class WidgetPool(object):
                 'The widget %s is not registered' % widget
             )
         del self.widgets[widget_name]
-        
+
     def get_widget(self, name):
         self.discover_widgets()
         return self.widgets[name]
-    
+
     def get_all_widgets(self, has_model=False, snippet=None):
         self.discover_widgets()
         widgets = [x for x in self.widgets.values() if x.model==SmartSnippetVariable]
         widgets.sort(key=lambda obj: unicode(obj.name))
         return widgets
+
+    def get_settings(self, namespace, name=None):
+        self.discover_widgets()
+        if not name:
+            return self._settings[namespace]
+        return self._settings[namespace].get(name, None)
 
 widget_pool = WidgetPool()
