2ac0965060c3b14b7679623dd6cfbca33ce4dcd4
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-10-27 16:38:24 +0200

    Added merge migration for parallel changes.

---
+++ smartsnippets/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('smartsnippets', '0003_change_smartsnippetvariable_description'),
+        ('smartsnippets', '0003_auto_20151008_1050'),
+    ]
+
+    operations = [
+    ]
